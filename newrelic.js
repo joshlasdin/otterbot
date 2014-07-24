df85378609d92b6f3ce8e3b69f6b886aceda9c66
Josh Lasdin <josh@mododesigngroup.com>
2014-07-24 15:37:43 -0400

    remove newrelic, using uptime robot instead

--- newrelic.js
+++
@@ -1,7 +0,0 @@
-exports.config = {
-    app_name : ['ias-otterbot'],
-    license_key : '612cfb81a621955f9af039edfacbeb1933b6671c',
-    logging : {
-        level : 'info'
-    }
-};
--- run.js
+++ run.js
@@ -1,5 +1,3 @@
-require('newrelic');
-
 var express = require('express'),
     otterbot = require('./bot');
 
