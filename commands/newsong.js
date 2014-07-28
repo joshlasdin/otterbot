501ac7fc2a30f299279290580734090b436d7fa9
Josh Lasdin <josh@mododesigngroup.com>
2014-07-28 08:51:33 -0400

    Ensure a song is playing before attempting to scrobble

--- commands/newsong.js
+++ commands/newsong.js
@@ -8,21 +8,23 @@ exports.init = function () {
     otterbot.on('djAdvance', function(data) {
         var song = data.media,
             time = new Date(data.mediaStartTime * 1000);
-            
-        otterbot.log(_.template('"<%= title %>" started playing. Waiting 30 seconds to scrobble it.', song));
-            
-        _.delay(function () {
-            if (otterbot.getMedia() === song) {
-                otterbot.log(_.template('"<%= title %>" still playing, scrobbling to last.fm.', song));
-                lastfm.scrobble(song, time, function (err) {
-                    if (err) {
-                        otterbot.log('"<%= title %>" Failed to scrobble error:');
-                        otterbot.log(err);
-                    } else {
-                        otterbot.log(_.template('"<%= title %>" successfully scrobbled.', song));
-                    }
-                });
-            }
-        }, 30000);
+
+        if (song.title) {
+            otterbot.log(_.template('"<%= title %>" started playing. Waiting 30 seconds to scrobble it.', song));
+
+            _.delay(function () {
+                if (otterbot.getMedia() === song) {
+                    otterbot.log(_.template('"<%= title %>" still playing, scrobbling to last.fm.', song));
+                    lastfm.scrobble(song, time, function (err) {
+                        if (err) {
+                            otterbot.log('"<%= title %>" Failed to scrobble error:');
+                            otterbot.log(err);
+                        } else {
+                            otterbot.log(_.template('"<%= title %>" successfully scrobbled.', song));
+                        }
+                    });
+                }
+            }, 30000);
+        }
     });
 };
