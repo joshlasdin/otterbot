c2a882023f4cf7680ee1883beff82f55d6bc42ea
Josh Lasdin <josh@mododesigngroup.com>
2014-07-29 08:59:18 -0400

    Testing gif search

---
+++ commands/gifsearch.js
@@ -0,0 +1,22 @@
+var otterbot = require('../bot'),
+    Helpers = require('../Helpers'),
+    request = require('request');
+
+exports.init = function () {
+    otterbot.on('chat', function (chat) {
+        var message = chat.message;
+
+        if (Helpers.matchString('contains', '.gif ', message)) {
+            var query = encodeURIComponent(message.replace('.gif ', ''));
+            request('http://wifflegif.com/search.json?q=' + query, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    otterbot.log('Got some gifs:');
+                    otterbot.log(body);
+                } else {
+                    otterbot.log('Couldn\'t get gifs:');
+                    otterbot.log(error);
+                }
+            });
+        }
+    });
+};
