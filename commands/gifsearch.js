b5452e28aa9ff2a56f4eaebc38b7bd9a4ae9237f
Josh Lasdin <josh@mododesigngroup.com>
2014-07-29 09:07:02 -0400

    Post gif to chat

--- commands/gifsearch.js
+++ commands/gifsearch.js
@@ -1,6 +1,7 @@
 var otterbot = require('../bot'),
     Helpers = require('../Helpers'),
-    request = require('request');
+    request = require('request'),
+    _ = require('lodash');
 
 exports.init = function () {
     otterbot.on('chat', function (chat) {
@@ -10,8 +11,12 @@ exports.init = function () {
             var query = encodeURIComponent(message.replace('.gif ', ''));
             request('http://wifflegif.com/gifs/search.json?q=' + query, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    otterbot.log('Got some gifs:');
-                    otterbot.log(body);
+                    var gifs = JSON.parse(body);
+                    if (_.isEmpty(gifs)) {
+                        otterbot.chatSingle(_.template('@<%= name %> Sorry, I couldn\'t find any gifs for "<%= query %>"', { name: chat.from, query: query }));
+                    } else {
+                        otterbot.chatSingle(_.random(gifs).url);
+                    }
                 } else {
                     otterbot.log('Couldn\'t get gifs:');
                     otterbot.log(error);
