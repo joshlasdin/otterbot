570a08a6151f8bf9e26df21f9e913ee3cd80d274
Josh Lasdin <joshlasdindesign@gmail.com>
2015-06-21 17:22:19 -0400

    Move initial run file.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express'),
-    otterbot = require('./bot');
+"use strict";
+
+var app = require('express')(),
+    port = +(process.env.PORT || 5000),
+    otterbot = require('bot/bot');
 
 // Set up webserver for pinging
-var app = express();
-
 app.get('/', function(req, res) { res.send('Hello World!'); });
 
 // Listen on port given by heroku
-var port = Number(process.env.PORT || 5000);
 app.listen(port, function() { console.log("Listening on " + port); });
 
 // Connect to the room
