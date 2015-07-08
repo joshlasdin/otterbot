517cd0dda93b333ac4a97877807d2838e39fd127
Jonathan Kupferman <jmkupferman@gmail.com>
2015-07-08 14:59:08 -0400

    Fix the decider so that it gives yes/no responses when there is only one option (e.g. should I eat?)

--- bot/commands/decider.js
+++ bot/commands/decider.js
@@ -19,8 +19,13 @@ exports.init = function () {
             // Split out the options, omitting the leading "Should I"
             options = message.toLowerCase().replace(/should i /g, '').split(' or ');
 
-            // Pick one of the options at random
-            otterbot.chatSingle(_.template('You should: <%= string %>', { string: options[_.random(options.length - 1)] }));
+            if(options.length > 1) {
+                // Pick one of the options at random
+                otterbot.chatSingle(_.template('You should: <%= string %>', { string: options[_.random(options.length - 1)] }));
+            } else if(options.length === 1) {
+              var options = ["Yes you should", "No you should not"];
+              otterbot.chatSingle(options[_.random(options.length - 1)])
+            }
         }
     });
 };
